6704859adb4ef920c30daceb2a8d2e90df78ff8d
ivanaan <67283786+ivanaan@users.noreply.github.com>
2020-07-30 23:25:25 -0400

    corrected wind value

--- src/script.js
+++ src/script.js
@@ -48,8 +48,8 @@ function showTemp(response) {
   city.innerHTML = response.data.name;
   let temperature = document.querySelector("#main-temp");
   temperature.innerHTML = Math.round(response.data.main.temp);
-  let wind = document.querySelector(Math.round("#wind"));
-  wind.innerHTML = response.data.wind.speed;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = Math.round(response.data.wind.speed);
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = response.data.main.humidity;
   let country = document.querySelector("#country");
